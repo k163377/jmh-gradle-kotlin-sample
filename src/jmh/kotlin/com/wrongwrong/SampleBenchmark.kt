5a025be0c526915852e889250ece90e04b9fe38f
wrongwrong <boranti1995@gmail.com>
2020-10-10 13:02:57 +0900

    最低限のベンチマークを追加

---
+++ src/jmh/kotlin/com/wrongwrong/SampleBenchmark.kt
@@ -0,0 +1,22 @@
+package com.wrongwrong
+
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.Scope
+import org.openjdk.jmh.annotations.State
+import java.math.BigDecimal
+import java.util.concurrent.ThreadLocalRandom
+
+open class SampleBenchmark {
+    @State(Scope.Thread)
+    open class Input {
+        val a: BigDecimal = ThreadLocalRandom.current().nextDouble(MAX_VALUE).toBigDecimal()
+        val b: BigDecimal = ThreadLocalRandom.current().nextDouble(MAX_VALUE).toBigDecimal()
+
+        companion object {
+            private const val MAX_VALUE = 10000.0
+        }
+    }
+
+    @Benchmark
+    fun measureAdd(input: Input): BigDecimal = input.a + input.b
+}
